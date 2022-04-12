066aa5056c0b9c62f3d8bdc1427a113130a35cc5
Se7en <yt.seven71@gmail.com>
2022-04-12 18:06:33 +0200

    Inventory param on create

--- src/ContainerMenu.ts
+++ src/ContainerMenu.ts
@@ -1,4 +1,4 @@
-import { FakeContainerType } from "./containers/FakeContainer";
+import { ContainerInventory, FakeContainer, FakeContainerType } from "./containers/FakeContainer";
 import { ChestContainer } from "./containers/ChestContainer";
 import { ServerPlayer } from "bdsx/bds/player";
 import { PlayerManager } from "./PlayerManager";
@@ -7,11 +7,17 @@ import { PacketListener } from "./listener/PacketListener";
 PacketListener.loadListeners();
 
 export namespace ContainerMenu {
-    export function create(player: ServerPlayer, container: FakeContainerType) {
+    /**
+     * Creates a fake container for a specific player.
+     *
+     * @param player - The player to create the container for.
+     * @param container - The container type to create.
+     */
+    export function create(player: ServerPlayer, container: FakeContainerType, inventory?: ContainerInventory): FakeContainer {
         if(!PlayerManager.hasContainer(player.getNetworkIdentifier())) {
             switch(container) {
                 case FakeContainerType.Chest:
-                    return new ChestContainer(player);
+                    return new ChestContainer(player, inventory);
             }
         } else throw new Error("Player already has a fake container assigned. Close it before creating a new one.");
     }
