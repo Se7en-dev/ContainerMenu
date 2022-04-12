4b0e89cb3b924c8081794994eaa33b5867124f8a
Se7en <yt.seven71@gmail.com>
2022-04-12 18:08:02 +0200

    Utils docstrings

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -5,13 +5,18 @@ import { Block } from "bdsx/bds/block";
 export namespace Utils {
     /**
      * TODO: return the correct position, not feet pos.
-     *
      */
     export function getBehindPosition(netId: NetworkIdentifier): BlockPos {
         const actor = netId.getActor()!;
         return BlockPos.create(actor.getFeetPos());
     }
 
+    /**
+     * Returns the original block at the given position.
+     *
+     * @param netId - The network identifier of the player.
+     * @param pos - The position to get the block from.
+     */
     export function getBlockAtPosition(netId: NetworkIdentifier, pos: BlockPos): Block {
         return netId.getActor()!.getRegion().getBlock(pos);
     }
